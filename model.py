9bdcf60981f98db7d2aef268e7bfb71c30bb26b8
GaddyW <gaddy125@gmail.com>
2019-01-22 13:10:06 +0000

    first draft

---
+++ model.py
@@ -0,0 +1,108 @@
+##Todos
+#read up on various models
+#skip the time I drove off the track
+#use right and left cameras - maybe
+#bird's eye view?
+#use generator
+
+##done
+#crop the image
+#flip images and invert direction for augmentation
+
+import csv
+import os
+import cv2
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+#import data
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in tqdm(reader):
+        lines.append(line)
+
+images = []
+angles = []
+counter = 0
+
+for line in lines:
+    if line[0] == "center":
+        continue
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    angle = float(line[3])
+    angles.append(angle)
+    counter += 1
+    if counter > 150: 
+        break
+
+
+
+
+# compile and train the model using the generator function
+#train_generator = generator(train_samples, batch_size=32)
+#validation_generator = generator(validation_samples, batch_size=32)    
+
+#preprocess data in batches in a generator
+#camera calibration?
+#bird's eye view?
+
+#augment data 
+augmented_images, augmented_angles = [], []
+for image, angle in zip(images, angles):
+    augmented_images.append(image)
+    augmented_angles.append(angle)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_angles.append(angle*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_angles)
+cv2.imwrite('test.jpg', augmented_images[10])
+#plt.imshow(X_train[0])
+
+#build network architecture
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers import Cropping2D
+
+
+model = Sequential()
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255 - 0.5))
+model.add(Flatten())
+model.add(Dense(1))
+
+#train network
+from keras.models import Model
+
+
+model.compile(loss='mse', optimizer = 'adam')
+history_object = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, epochs = 5, verbose = 1)
+#history_object = model.fit_generator(train_generator, samples_per_epoch =
+#    len(train_samples), validation_data = 
+#    validation_generator,
+#    nb_val_samples = len(validation_samples), 
+#    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+#plt.plot(history_object.history['val_loss'])
+#plt.title('model mean squared error loss')
+#plt.ylabel('mean squared error loss')
+#plt.xlabel('epoch')
+#plt.legend(['training set', 'validation set'], loc='upper right')
+#plt.show()
+
+#save model
+model.save('model.h5')
